556a61ccfa4887ececd9fc6188b513c0cb52d8cc
haven <85509162+cookiesock@users.noreply.github.com>
2024-08-15 08:15:18 -0500

    upgrade to godot 4.3 + slight movement changes

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,12 +1,10 @@
 extends CharacterBody3D
 
-
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
-
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -14,18 +12,17 @@ func _physics_process(delta):
 		velocity.y -= gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("move_jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input_dir = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = lerpf(velocity.x, direction.x * SPEED, clamp(0.32 * 60 * delta, 0, 1))
+		velocity.z = lerpf(velocity.z, direction.z * SPEED, clamp(0.32 * 60 * delta, 0, 1))
 
 	move_and_slide()
