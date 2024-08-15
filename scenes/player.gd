10716c12bf210d2623565147bb00cedad292c601
Raf <85767304+RafGamign@users.noreply.github.com>
2024-08-15 13:34:18 -0300

    new tv shader + default imports + renamed actions

--- scenes/player.gd
+++ scenes/player.gd
@@ -12,11 +12,11 @@ func _physics_process(delta):
 		velocity.y -= gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("move_jump") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
-	var input_dir = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	var input_dir = Input.get_vector("walk_left", "walk_right", "walk_up", "walk_down")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
