4a1cf8742931b5dfa89a6e9c08fc41402221d772
Raf <85767304+RafGamign@users.noreply.github.com>
2024-08-17 01:00:25 -0300

    THEY'RE REAL!!!!!!!!!!!!!
    - face
    - screen light
    - animations
    - bigger ground for em to walk on :3
    - removed unused fonts
    - updated their textures (head and the unused face texture)
    - merged images on the same place
    - made the model scene inherited
    - smooth rotation and movement in general
    - probably some other shit i forgot

--- src/player.gd
+++ src/player.gd
@@ -2,6 +2,7 @@ extends CharacterBody3D
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
+var look_dir = Vector2.UP
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -18,9 +19,15 @@ func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	var input_dir = Input.get_vector("walk_left", "walk_right", "walk_front", "walk_back")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	velocity.x = lerp(velocity.x, direction.x * SPEED, 0.20 * 60 * delta)
-	velocity.z = lerp(velocity.z, direction.z * SPEED, 0.20 * 60 * delta)
+	velocity.x = lerp(velocity.x, direction.x * SPEED, 0.10 * 60 * delta)
+	velocity.z = lerp(velocity.z, direction.z * SPEED, 0.10 * 60 * delta)
 
 	move_and_slide()
-	if velocity.x or velocity.z:
-		$Model.rotation.y = -PI/2.0-Vector2(velocity.x,velocity.z).angle()
+	if input_dir:
+		$Model.rotation.y = lerp_angle(
+			$Model.rotation.y,
+			-PI/2.0-input_dir.angle(),
+			0.2 * 60 * delta
+		)
+	
+	$Model/AnimationPlayer.play("walk" if Vector2(velocity.x,velocity.z).length() > 0.1 and input_dir else "idle")
