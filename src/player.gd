fe0c79b27c8e7a98aa970bb53a9b5f8e1a82fc48
Raf <85767304+RafGamign@users.noreply.github.com>
2024-08-17 01:23:59 -0300

    pressure sensitive input

--- src/player.gd
+++ src/player.gd
@@ -18,7 +18,7 @@ func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
 	var input_dir = Input.get_vector("walk_left", "walk_right", "walk_front", "walk_back")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y))
 	velocity.x = lerp(velocity.x, direction.x * SPEED, 0.10 * 60 * delta)
 	velocity.z = lerp(velocity.z, direction.z * SPEED, 0.10 * 60 * delta)
 
